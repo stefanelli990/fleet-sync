0ae03b38d3d6b2765ad6274a9fdd9a95827c4c55
stefanelli990 <stefan.mohenski@gmail.com>
2021-06-22 19:45:03 +0200

    last change

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,8 @@
 const loader = document.querySelector('.loader');
 const body = document.querySelector('body');
 
-
-
   //loader
   setTimeout(() => {
     loader.classList.add('active');
-   
 }, 2000);
+
