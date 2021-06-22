1cca3da359f54370f45b4e68c4e5a6e4eb9a0b66
stefanelli990 <stefan.mohenski@gmail.com>
2021-06-22 04:56:20 +0200

    odradjen login i get started

---
+++ script.js
@@ -0,0 +1,10 @@
+const loader = document.querySelector('.loader');
+const body = document.querySelector('body');
+
+
+
+  //loader
+  setTimeout(() => {
+    loader.classList.add('active');
+   
+}, 2000);
